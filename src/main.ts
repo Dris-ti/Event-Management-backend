3f2491f329254fc2b644cfd65e73ddf1db9a7a17
Dristi <usrdristi@gmail.com>
2025-05-23 05:04:56 +0600

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    app.enableCors({
+    origin: 'http://localhost:3001', // Must match your frontend domain
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
